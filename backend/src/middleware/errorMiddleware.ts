3f39b01f8d7d8361f89adc29fc0417bbdf4cfae0
jalva <pjalva90@gmail.com>
2026-05-15 10:08:24 +0530

    chore: add missing types for jwt, morgan, and nodemailer

--- backend/src/middleware/errorMiddleware.ts
+++ backend/src/middleware/errorMiddleware.ts
@@ -5,6 +5,7 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
     const statusCode = res.statusCode === STATUS_CODE.SUCCESS ? STATUS_CODE.INTERNAL_SERVER_ERROR : res.statusCode;
     res.status(statusCode);
     res.json({
+        
         message: err.message,
         stack: process.env.NODE_ENV === 'production' ? null : err.stack,
     });
